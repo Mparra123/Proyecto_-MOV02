0396483cbb6c02ca5fa304fddbe84074c5cfb3a8
Mparra123 <mauricioparrales@hotmail.com>
2020-08-25 08:33:54 -0600

    Initial Commit

---
+++ Proyecto_ MOV02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Proyecto_ MOV02
+//
+//  Created by Mauricio Parrales on 8/25/20.
+//  Copyright © 2020 test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
